661b84c297a035cd4e584504d9fe8cdce3372997
unknown <220253317@tut4life.ac.za>
2023-09-14 13:47:48 +0200

    At last

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const app = express();
+
+app.set('view engine', 'ejs'); // Set EJS as the template engine
+app.set('views', __dirname + '/views'); // Set the views directory
+
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+app.use(express.static(__dirname + '/public'));
 
 // MySQL Connections
 const db = mysql.createConnection({
@@ -25,11 +32,26 @@ db.connect(err => {
 
 // Routes
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.render('register');
 });
 
+
+
 app.post('/register', (req, res) => {
-  const { username, password } = req.body;
+  const { username, password, password2 } = req.body;
+
+  // Check if passwords match
+  if (password !== password2) {
+    return res.status(400).send('Passwords do not match');
+  }
+
+  app.use(
+    session({
+      secret: 'your-secret-key', // Replace with a secure secret key
+      resave: true,
+      saveUninitialized: true,
+    })
+  );
 
   db.query(
     'INSERT INTO users (username, password) VALUES (?, ?)',
@@ -40,11 +62,21 @@ app.post('/register', (req, res) => {
         res.status(500).send('Internal Server Error');
       } else {
         res.append('Registration Successful');
+        res.redirect('/login');
       }
     }
   );
 });
 
+
+app.post('/main', (req, res) => {
+  res.render('main')
+});
+app.get('/login', (req, res) => {
+  // Render the 'login.ejs' template and pass data if needed
+  res.render('login', { message: 'Already a member?' });
+  
+});
 // Start the server
 const port = process.env.PORT || 3030;
 app.listen(port, () => {
